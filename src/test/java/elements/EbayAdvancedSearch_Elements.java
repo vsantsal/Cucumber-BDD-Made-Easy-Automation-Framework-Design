bcb0f0ebc8894355ac2528d57f531ba75eac705a
vsantsal <vsantsal@gmail.com>
2025-09-20 22:47:14 -0300

    bugfix: altera seletor de logo Ebay e corrige typo no nome da classe

--- src/test/java/elements/EbayAdvancedSearch_Elements.java
+++ src/test/java/elements/EbayAdvancedSearch_Elements.java
@@ -5,18 +5,18 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class EabyAdvancedSearch_Elements {
+public class EbayAdvancedSearch_Elements {
 	
 	WebDriver driver;
 	
-	@FindBy(xpath = "//a[@id='gh-la']") public WebElement ebayLogo;
+	@FindBy(xpath = "//a[@class='gh-logo']") public WebElement ebayLogo;
 	@FindBy(xpath = "//input[@id='_nkw']") public WebElement searchString;
 	@FindBy(xpath = "//input[@id='_ex_kw']") public WebElement excludeString;
 	@FindBy(xpath = "//input[@name='_udlo']") public WebElement minPrice;
 	@FindBy(xpath = "//input[@name='_udhi']") public WebElement maxPrice;
 	@FindBy(xpath = "//button[@id='searchBtnLowerLnk']") public WebElement srchBtn;
 	
-	public EabyAdvancedSearch_Elements(WebDriver driver) {
+	public EbayAdvancedSearch_Elements(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
