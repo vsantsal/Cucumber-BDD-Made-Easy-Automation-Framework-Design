9aa8b0f3e80fd88b14820e01d8d39ca1fae005d7
vsantsal <vsantsal@gmail.com>
2025-09-20 23:17:06 -0300

    feat: implementa passos do novo cenario

--- src/test/java/steps/EbayHome_Steps.java
+++ src/test/java/steps/EbayHome_Steps.java
@@ -2,8 +2,6 @@ package steps;
 
 import static org.junit.Assert.fail;
 
-
-
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Entao;
 import io.cucumber.java.pt.Quando;
@@ -11,8 +9,6 @@ import io.cucumber.java.pt.Quando;
 import actions.Common_Actions;
 import actions.EbayHome_Actions;
 
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
 
 public class EbayHome_Steps {
 	Common_Actions common_actions;
@@ -23,8 +19,8 @@ public class EbayHome_Steps {
 		this.ebayhome_actions = ebayhome_actions;
 	}
 	
-	@Dado("Estou na Home Page do Ebay")
-	public void estouNaHomePageDoEbay() {
+	@Dado("Estou na Home Page da Ebay")
+	public void estouNaHomePageDaEbay() {
 	   common_actions.goToUrl("https://www.ebay.com/");
 	}
 
@@ -42,44 +38,44 @@ public class EbayHome_Steps {
 	    }
 	}
 	
-	@When("I serach for {string}")
-	public void i_serach_for_iPhone_11(String str1) throws Exception {
+	@Quando("Pesquiso por {string}")
+	public void pesquisoPorTexto(String str1) throws Exception {
 		ebayhome_actions.searchAnItem(str1);
 		ebayhome_actions.clickSearchButton();
 		Thread.sleep(1000);
 	}
 
-	@Then("I validate atleast {int} search items present")
-	public void i_validate_atleast_search_items_presentint (int count) {
+	@Entao("Valido que pelo menos {int} itens foram encontrados")
+	public void validoQuePeloMenosAlgunsItensForamEncontrados (int count) {
 	    int itemCountInt = ebayhome_actions.getSeatchItemsCount();
 	    if(itemCountInt <= count) {
-	    	fail("Less than 1000 results shown");
+	    	fail("Foram encontrados menos de " + count + " itens");
 	    }
 	}
 	
-	@When("I serach for {string} in {string} category")
-	public void i_serach_for_in_category(String string, String string2) throws Exception {
-		ebayhome_actions.searchAnItem(string);
-		ebayhome_actions.selectCategoryOption(string2);
-		ebayhome_actions.clickSearchButton();
-		Thread.sleep(1000);
-	}
-	
-	@When("I click on {string}")
-	public void i_click_on(String string) throws Exception {
-	   ebayhome_actions.clickOnLinkByText(string);
-	   Thread.sleep(1000);
-	}
-
-	@Then("I validate that page navigates to {string} and title contains {string}")
-	public void i_validate_that_page_navigates_to_and_title_contains(String url, String title) {
-	    String actUrl = common_actions.getCurrentPageUrl();
-	    String actTitle = common_actions.getCurrentPageTitle();
-	    if (!actUrl.equals(url)) {
-	    	fail("Page does navigate to expected url: " + url);
-	    }
-	    if (!actTitle.contains(title)) {
-	    	fail("Title mismatch");
-	    }
-	}
+//	@When("I serach for {string} in {string} category")
+//	public void i_serach_for_in_category(String string, String string2) throws Exception {
+//		ebayhome_actions.searchAnItem(string);
+//		ebayhome_actions.selectCategoryOption(string2);
+//		ebayhome_actions.clickSearchButton();
+//		Thread.sleep(1000);
+//	}
+//
+//	@When("I click on {string}")
+//	public void i_click_on(String string) throws Exception {
+//	   ebayhome_actions.clickOnLinkByText(string);
+//	   Thread.sleep(1000);
+//	}
+//
+//	@Then("I validate that page navigates to {string} and title contains {string}")
+//	public void i_validate_that_page_navigates_to_and_title_contains(String url, String title) {
+//	    String actUrl = common_actions.getCurrentPageUrl();
+//	    String actTitle = common_actions.getCurrentPageTitle();
+//	    if (!actUrl.equals(url)) {
+//	    	fail("Page does navigate to expected url: " + url);
+//	    }
+//	    if (!actTitle.contains(title)) {
+//	    	fail("Title mismatch");
+//	    }
+//	}
 }
